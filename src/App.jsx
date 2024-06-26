6eb1650e6c9f0ddf3cc6d8c75f63140318e94f0a
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-06-26 15:41:38 +0200

    + axios and trips.json fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,28 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Components/Header";
-
-const trips = [
-  {
-    id: 1,
-    title: "BT01",
-    description: "San Francisco World Trade Center on new Server/IOT/Client ",
-    startTrip: [2021, 2, 13, 0, 0],
-    endTrip: [2021, 2, 15, 16, 56],
-    meetings: [
-      {
-        id: 1,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-      {
-        id: 2,
-        title: "Zero Conference",
-        description: "Workshop Zero on One Conference",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "BT02",
-    description: "Santa Clara Halley on new Server/IOT/Client",
-    startTrip: [2021, 6, 23, 9, 0],
-    endTrip: [2021, 6, 27, 16, 56],
-    meetings: [
-      {
-        id: 3,
-        title: "One Conference",
-        description: "HandsOn on One Conference",
-      },
-      {
-        id: 4,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "BT03",
-    description: "San Cose City Halley on Docker/IOT/Client",
-    startTrip: [2021, 12, 13, 9, 0],
-    endTrip: [2021, 12, 15, 16, 56],
-    meetings: [
-      {
-        id: 5,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-    ],
-  },
-];
+import axios from "axios";
 
 const selectedTrips = [];
 
 export default function App() {
   const [snackbarVisible, setSnackbarVisible] = useState(false);
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      try {
+        const response = await axios.get("./trips.json");
+        setTrips(response.data);
+      } catch (error) {
+        console.error("Error fetching trips:", error);
+      }
+    };
+
+    fetchTrips();
+  }, []);
+
 
   const showSnackbar = () => {
     setSnackbarVisible(true);
