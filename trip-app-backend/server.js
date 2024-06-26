e3b35ab77534ec0259c84519f815cc8fa9a9e060
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-06-26 18:52:19 +0200

    Revert "before trying cors"
    This reverts commit 94b380ed96c28cd6fe5d667acf844773ddf3afe3.

--- trip-app-backend/server.js
+++ trip-app-backend/server.js
@@ -10,69 +10,55 @@ const tripsFilePath = path.join(__dirname, './trips.json');
 app.use(bodyParser.json());
 
 // Read trips
-app.get('/trips', async (req, res) => {
-    try {
-        console.log(1);
-        const data = await readFileAsync(tripsFilePath);
-        console.log(2);
-        res.json(JSON.parse(data));
-    } catch (err) {
-        console.error('Error reading trips file:', err);
-        res.status(500).send('Error reading trips file');
-    }
+app.get('/trips', (req, res) => {
+    fs.readFile(tripsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading trips file');
+        }
+        res.send(JSON.parse(data));
+    });
 });
 
 // Delete a trip
-app.delete('/trips/:id', async (req, res) => {
+app.delete('/trips/:id', (req, res) => {
     const tripId = parseInt(req.params.id);
-    try {
-        let data = await readFileAsync(tripsFilePath);
+    fs.readFile(tripsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading trips file');
+        }
         const trips = JSON.parse(data).trips;
         const newTrips = trips.filter(trip => trip.id !== tripId);
 
-        await writeFileAsync(tripsFilePath, JSON.stringify({ trips: newTrips }));
-        res.json({ success: true });
-    } catch (err) {
-        console.error('Error deleting trip:', err);
-        res.status(500).send('Error deleting trip');
-    }
+        fs.writeFile(tripsFilePath, JSON.stringify({ trips: newTrips }), err => {
+            if (err) {
+                return res.status(500).send('Error writing to trips file');
+            }
+            res.send({ success: true });
+        });
+    });
 });
 
-// Add a trip
-app.post('/trips', async (req, res) => {
+// Add a trip (for completeness)
+app.post('/trips', (req, res) => {
     const newTrip = req.body;
-    try {
-        let data = await readFileAsync(tripsFilePath);
+    fs.readFile(tripsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading trips file');
+        }
         const trips = JSON.parse(data).trips;
         trips.push(newTrip);
 
-        await writeFileAsync(tripsFilePath, JSON.stringify({ trips }));
-        res.json({ success: true });
-    } catch (err) {
-        console.error('Error adding trip:', err);
-        res.status(500).send('Error adding trip');
-    }
+        fs.writeFile(tripsFilePath, JSON.stringify({ trips }), err => {
+            if (err) {
+                return res.status(500).send('Error writing to trips file');
+            }
+            res.send({ success: true });
+        });
+    });
 });
-
-// Async file read/write functions
-const readFileAsync = (filePath) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) reject(err);
-            else resolve(data);
-        });
-    });
-};
-
-const writeFileAsync = (filePath, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filePath, data, 'utf8', (err) => {
-            if (err) reject(err);
-            else resolve();
-        });
-    });
-};
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}/trips`);
 });
+
+
