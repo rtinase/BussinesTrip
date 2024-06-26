840eed8123a1c974bfab14f96b0c151114b0cedc
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-06-26 14:11:27 +0200

    + bakcHome button

--- src/pages/MyTrips.jsx
+++ src/pages/MyTrips.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function MyTrips() {
+    const navigate = useNavigate();
+
+    function returnToHome() {
+        navigate("/");
+    }
+
     return (
         <div>
             <h1>My Trips Page</h1>
+            <button onClick={returnToHome}>Back home</button>
         </div>
     );
 };
