8cd3f182f4fe5fb21eac1ecce326e468479e3d62
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-07-10 15:33:45 +0200

    + styling

--- frontend/src/pages/MyTrips/TripList.jsx
+++ frontend/src/pages/MyTrips/TripList.jsx
@@ -5,7 +5,14 @@ import "./MyTrips.css";
 const TripList = ({ trips, onDeleteTrip }) => {
     return (
         <div className="trip-list">
-            {trips.map((trip) => (
+            <div className="trip-row trip-header">
+                <span className="trip-id">ID</span>
+                <span className="trip-title">Title</span>
+                <span className="trip-date">Start Date</span>
+                <span className="trip-date">End Date</span>
+                <span className="trip-actions">Actions</span>
+            </div>
+            {trips.map(trip => (
                 <Trip key={trip.id} trip={trip} onDeleteTrip={onDeleteTrip} />
             ))}
         </div>
